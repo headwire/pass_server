89995c2e65a23f640c5ba7dd1772e39bb8e55b8c
headwire <bp.headwire@gmail.com>
2014-05-04 23:07:15 +0300

    Zip::OutputStream fix

--- lib/sign_pass.rb
+++ lib/sign_pass.rb
@@ -201,7 +201,7 @@ class SignPass
     puts "Compressing the pass"
     zipped_file = File.open(self.output_url, "w")
 byebug
-    Zip::ZipOutputStream.open(zipped_file.path) do |z|
+    Zip::OutputStream.open(zipped_file.path) do |z|
       Dir.glob(self.temporary_path + "/**").each do |file|
         z.put_next_entry(File.basename(file))
         z.print IO.read(file)
